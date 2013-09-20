d3ce08df760faa8e8a4971a977b7c8fca9eff4c5
swee17hp <swee17_hp@yahoo.com>
2013-09-20 11:41:45 +0800

    Final commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'activerecord-tableless'
+gem 'bootstrap-sass', '~> 2.3.2.2'
+gem 'figaro'
+gem 'gibbon'
+gem 'google_drive'
+gem 'high_voltage'
+gem 'simple_form', '>= 3.0.0.rc'
+group :development do
+	gem 'better_errors'
+	gem 'quiet_assets'
+	gem 'sqlite3'
+end
+group :production do
+	gem 'pg'
+	gem 'rails_12factor'
+	gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnRails::Application.config.secret_key_base = '9d7d19924219e9f3da85caea4e42ff37c15f799ba72f56b133cb3203a6f1a2f9a6128c6b720ad746341d564508d154735c161c2b29ffaf765fa5ae96d99d353a'
